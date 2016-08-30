bd2a2675b6f5cc86f8f73fb28b8bd5aef8b59875
shyun <shyun@shyun-PC>
2016-08-30 10:20:21 +0900

    Connection 작업 분리

--- Chapter_1/src/com/spring/domain/UserDao.java
+++ Chapter_1/src/com/spring/domain/UserDao.java
@@ -10,8 +10,7 @@ public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "root");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -27,8 +26,7 @@ public class UserDao {
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "root");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id=?");
 		ps.setString(1, id);
@@ -45,6 +43,16 @@ public class UserDao {
 		c.close();
 
 		return user;
+	}
+
+	
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "root");
+		
+		return  c;
+
 	}
 
 }
