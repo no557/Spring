3dda41bdce5c0acc3e2d8c385adb0264ac1c1249
shyun <shyun@shyun-PC>
2016-08-30 10:11:45 +0900

    DAO(add,get) 생성. maven 추가 .

--- Chapter_1/src/com/spring/app/Main.java
+++ Chapter_1/src/com/spring/app/Main.java
@@ -1,11 +1,32 @@
 package com.spring.app;
 
+import java.sql.SQLException;
+
+import com.spring.domain.User;
+import com.spring.domain.UserDao;
+
 public class Main {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		
+		user.setId("root");
+		user.setName("shyun");
+		user.setPassword("root");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + "add success ");
+		
+		User user2 = dao.get(user.getId());
+
+		System.out.println(user2.getId() + "get success ");
+		
 	
-		System.out.println("Hello World");
 	}
 
 }
