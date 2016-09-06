bb5ba57ee60fe2a3358023f561815571145da83b
shyun <shyun@shyun-PC>
2016-09-06 16:50:58 +0900

    메일서비스 추상화

--- Chapter_1/src/com/spring/service/UserService.java
+++ Chapter_1/src/com/spring/service/UserService.java
@@ -1,16 +1,21 @@
 package com.spring.service;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
 
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.spring.domain.User;
 import com.spring.domain.UserDao;
@@ -21,14 +26,14 @@ public class UserService {
 	private UserDao userDao;
 	private DataSource dataSource;
 	private PlatformTransactionManager txManager;
-	
-
+    private MailSender mailSender;
+    
+    
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
-	
-	public void setDataSource(DataSource dataSource)
-	{
+
+	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 
@@ -36,53 +41,70 @@ public class UserService {
 		this.txManager = txManager;
 	}
 
-	//트렌젝션이 필요한 부분 
+	// 트렌젝션이 필요한 부분
 	public void upgradeLevels() throws SQLException {
-		
+
 		TransactionStatus status = this.txManager.getTransaction(new DefaultTransactionDefinition());
 		try {
-			
-		List<User> users = userDao.getAll();
 
-		for (User user : users) {
-			if(canUpgradeLevel(user)){
-				upgradeLevel(user);
-			}
-		}//end for 
-		
-		this.txManager.commit(status);
+			List<User> users = userDao.getAll();
+
+			for (User user : users) {
+				if (canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
+			} // end for
+
+			this.txManager.commit(status);
 
 		} catch (Exception e) {
 			// TODO: handle exception
-			
+
 			this.txManager.rollback(status);
-			throw e ;
+			throw e;
 
 		}
 	}
-	
-	public void add(User user){
-		if(user.getLevel()==null) user.setLevel(Level.BASIC);
+
+	public void add(User user) {
+		if (user.getLevel() == null)
+			user.setLevel(Level.BASIC);
 		userDao.add(user);
 	}
-	
-	protected void upgradeLevel(User user){
+
+	protected void upgradeLevel(User user) {
 		user.upgradeLevel();
 		userDao.update(user);
+		sendUpgradeEMail(user);
 	}
-	
+
 	private boolean canUpgradeLevel(User user) {
-		
+
 		Level currentLevel = user.getLevel();
 
 		switch (currentLevel) {
-		case BASIC: return (user.getLogin()>=50);
-		case SILVER:return (user.getRecommend()>=30);
-		case GOLD: return false;
-		default: throw new AssertionError("unknown level: "+ currentLevel);
+		case BASIC:
+			return (user.getLogin() >= 50);
+		case SILVER:
+			return (user.getRecommend() >= 30);
+		case GOLD:
+			return false;
+		default:
+			throw new AssertionError("unknown level: " + currentLevel);
 		}
-		
+
 	}
-	
+
+	private void sendUpgradeEMail(User user) {
+		
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(user.getEmail());
+		message.setFrom("n87929@gmail.com");
+		message.setSubject("Upgrade 안내");
+		message.setText("등급 업그레이드 : " + user.getLevel().name());
+
+		this.mailSender.send(message);
+	}
 
 }
